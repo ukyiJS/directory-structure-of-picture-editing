3070af479532bdf1a5d1bfe5f91ea6704136b9a8
UkYi <ukyi.js@gmail.com>
2020-10-28 02:49:29 +0900

    feat: type 추가

---
+++ src/type.ts
@@ -0,0 +1,23 @@
+export const JPG = 'jpg';
+export type JPG = typeof JPG;
+
+export const RAW = 'raw';
+export type RAW = typeof RAW;
+
+export const RESET = '\x1b[0m';
+export type RESET = typeof RESET;
+export const BRIGHT = '\x1b[1m';
+export type BRIGHT = typeof BRIGHT;
+export const UNDERSCORE = '\x1b[4m';
+
+export const RED = '\x1b[31m';
+export type RED = typeof RED;
+export const GREEN = '\x1b[32m';
+export type GREEN = typeof GREEN;
+export const YELLOW = '\x1b[33m';
+export type YELLOW = typeof YELLOW;
+export const CYAN = `\x1b[36m`;
+export type CYAN = typeof CYAN;
+
+export const ORIGINAL_DOCUMENT = '원본';
+export type ORIGINAL_DOCUMENT = typeof ORIGINAL_DOCUMENT;
