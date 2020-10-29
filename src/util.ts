8ec463ebf675a3ec83a9fdf9e23518a16a6aee72
UkYi <ukyi.js@gmail.com>
2020-10-29 17:59:26 +0900

    feat: underscore, bright 추가

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
-import { CYAN, GREEN, RED, RESET, YELLOW } from './type';
+import { BRIGHT, CYAN, GREEN, RED, RESET, UNDERSCORE, YELLOW } from './type';
 
 export const Logger = {
   info: (message?: any, ...optionalParams: any[]) => console.log(`${CYAN}${message}`, ...optionalParams, RESET),
@@ -6,3 +6,23 @@ export const Logger = {
   warn: (message?: any, ...optionalParams: any[]) => console.log(`${YELLOW}${message}`, ...optionalParams, RESET),
   error: (message?: any, ...optionalParams: any[]) => console.log(`${RED}${message}`, ...optionalParams, RESET),
 };
+
+type messageType = 'cyan' | 'green' | 'yellow' | 'red';
+
+const getType = (type?: messageType): string => {
+  switch (type) {
+    case 'cyan':
+      return CYAN;
+    case 'green':
+      return GREEN;
+    case 'yellow':
+      return YELLOW;
+    case 'red':
+      return RED;
+    default:
+      return '';
+  }
+};
+
+export const underscore = (message: string, type?: messageType) => `${UNDERSCORE}${getType(type)}${message}${RESET}`;
+export const bright = (message: string, type?: messageType) => `${BRIGHT}${getType(type)}${message}${RESET}`;
